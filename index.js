279befe528068fd2b5ce9ca1c92940c3bcfbfaf9
Moshe Maizels <moshemaizels@gmail.com>
2017-08-22 16:55:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,21 +11,18 @@ function increaseRankBy(n) {
  console.log("first")
   //var rankedList = ranked.querySelectorAll("li")
   var ranked = document.getElementById("app").querySelectorAll('ul.ranked-list')
-  console.log("ranked length " + ranked.length)
+// console.log(ranked.length)
   for (var i = 0; i < ranked.length; i++) {
     var rankedInside = ranked[i].querySelectorAll('li')
-    console.log("loop " + i)
-    for (var u = 0; u <2; u++) {
-      console.log("2nd loop" + i)
-    }
+    //console.log("loop " + i)
     //console.log(rankedInside.length)
     //var r = ranked[i].querySelectorAll('li')
-    /*for (var i = 0; i < rankedInside.length; i++) {
+    for (var u = 0; u < rankedInside.length; u++) {
       //console.log(parseInt(rankedInside[i].innerHTML) + n)
       rankedInside[i].innerHTML = parseInt(rankedInside[i].innerHTML) + n
     //  console.log("here")
     //  r.querySelectorAll('li')[i].innerHTML = parseInt(r.querySelectorAll('li')[i].innerHTML)+ n
-  }*/
+    }
     //ranked[i].innerHTML = parseInt(ranked[i].innerHTML) + n
 
   }
